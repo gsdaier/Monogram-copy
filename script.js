f6484c97d63425b0e1797cf65ed36f733acfa76d
Gustavo Daier <96262128+gsdaier@users.noreply.github.com>
2022-02-20 01:34:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function changePic1() {
+    document.getElementById("myImg").src = "./desktop images/imagehover.png";
+       }
+      function changePic2(){
+       document.getElementById("myImg").src = "./desktop images/travelerconsole.png";
+       }
+
+function changePic3() {
+        document.getElementById("myImg2").src = "./desktop images/imagehover.png";
+           }
+          function changePic4(){
+           document.getElementById("myImg2").src = "./desktop images/studioconsole.png";
+           }
+
+function changePic5() {
+            document.getElementById("myImg3").src = "./desktop images/masterhover.png";
+               }
+              function changePic6(){
+               document.getElementById("myImg3").src = "./desktop images/masterconsole.png";
+               }
