fc8d8b15bcf0e35eba616e083bb9c992a552c52d
Gustavo Daier <96262128+gsdaier@users.noreply.github.com>
2022-02-20 15:02:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,62 @@
 function changePic1() {
-    document.getElementById("myImg").src = "./desktop images/imagehover.png";
-       }
-      function changePic2(){
-       document.getElementById("myImg").src = "./desktop images/travelerconsole.png";
-       }
+   document.getElementById("myImg").src = "./desktop images/imagehover.png";
+}
+function changePic2() {
+   document.getElementById("myImg").src = "./desktop images/travelerconsole.png";
+}
 
 function changePic3() {
-        document.getElementById("myImg2").src = "./desktop images/imagehover.png";
-           }
-          function changePic4(){
-           document.getElementById("myImg2").src = "./desktop images/studioconsole.png";
-           }
+   document.getElementById("myImg2").src = "./desktop images/imagehover.png";
+}
+function changePic4() {
+   document.getElementById("myImg2").src = "./desktop images/studioconsole.png";
+}
 
 function changePic5() {
-            document.getElementById("myImg3").src = "./desktop images/masterhover.png";
-               }
-              function changePic6(){
-               document.getElementById("myImg3").src = "./desktop images/masterconsole.png";
-               }
+   document.getElementById("myImg3").src = "./desktop images/masterhover.png";
+}
+function changePic6() {
+   document.getElementById("myImg3").src = "./desktop images/masterconsole.png";
+}
+
+function changePic7() {
+   document.getElementById("myImg4").src = "./desktop images/modules/monogramcorehover.png" ;
+}
+function changePic8() {
+   document.getElementById("myImg4").src = "./desktop images/modules/monogramcore.png";
+}
+
+function changePic9() {
+   document.getElementById("myImg5").src = "./desktop images/modules/monogramcorehover.png";
+}
+function changePic10() {
+   document.getElementById("myImg5").src = "./desktop images/modules/orbitermodule.png";
+}
+
+function changePic11() {
+   document.getElementById("myImg6").src = "./desktop images/modules/monogramcorehover.png";
+}
+function changePic12() {
+   document.getElementById("myImg6").src = "./desktop images/modules/monogramcore.png";
+}
+
+function changePic13() {
+   document.getElementById("myImg7").src = "./desktop images/modules/monogramcarehover.png";
+}
+function changePic14() {
+   document.getElementById("myImg7").src = "./desktop images/modules/monogramcare.png";
+}
+
+function changePic15() {
+   document.getElementById("myImg8").src = "./desktop images/modules/monogramcorehover.png";
+}
+function changePic16() {
+   document.getElementById("myImg8").src = "./desktop images/modules/essential-keys-module.png";
+}
+
+function changePic17() {
+   document.getElementById("myImg9").src = "./desktop images/modules/monogramcorehover.png";
+}
+function changePic18() {
+   document.getElementById("myImg9").src = "./desktop images/modules/slidermodule.png";
+}
